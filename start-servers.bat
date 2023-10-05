d72aac6ac0b131c72f1445f1568f94af1cd4ef29
Moon2896 <baptiste.lesne@gmail.com>
2023-10-05 20:33:09 +0200

    UPDATED: Swap python/server + create output.csv

--- start-servers.bat
+++ start-servers.bat
@@ -1,9 +1,9 @@
 @echo off
 
-REM Start the python script
-start /B python ".\python_implem\EA.py" -i ".\python_implem\datasets\worldcities_10k.json" -o ".\python_implem\output.csv" -r ".\python_implem\results"
-REM Wait for a moment to ensure output.csv is created (adjust the wait time as needed)
-timeout /T 10 >nul
+REM Check if output.csv exists, if not create an empty one
+if not exist ".\python_implem\output.csv" (
+    echo. > .\python_implem\output.csv
+)
 
 REM Start the Node.js Express.js server on port 3000
 start /B node ".\python_implem\server\Express.js"
@@ -14,6 +14,14 @@ cd ".\tsp-ol-app\"
 start /B npm run start
 REM Wait for a moment to allow the OL application server to start
 timeout /T 2 >nul
+
+cd "../"
+
+REM Start the python script
+start /B python ".\python_implem\EA.py" -i ".\python_implem\datasets\worldcities_10k.json" -o ".\python_implem\output.csv" -r ".\python_implem\results"
+REM Wait for a moment to ensure output.csv is created (adjust the wait time as needed)
+timeout /T 10 >nul
+
 
 echo Press any key to stop the servers...
 pause >nul
